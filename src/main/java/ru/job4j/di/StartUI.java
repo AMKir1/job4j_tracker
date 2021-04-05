663c3db1e77e64c8fee77432bf4416517fab2bda
Andrei Kirillovykh <andykirill@gmail.com>
2021-04-05 09:48:46 +0300

    0. Что такое DI?[6876#453854]

---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,22 @@
+package ru.job4j.di;
+
+public class StartUI {
+
+    private Store store;
+    private ConsoleInput input;
+
+    public StartUI(Store store, ConsoleInput input) {
+        this.store = store;
+        this.input = input;
+    }
+
+    public void add() {
+        store.add(this.input.ask("What is your whole name: "));
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
